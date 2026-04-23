115bcf638a93d232c8b5434167f5f91b34beb0fa
marchelinho <luka.stojanovic212@gmail.com>
2026-04-23 17:45:59 +0200

    tipovi podataka

---
+++ TipoviPodataka/main.py
@@ -0,0 +1,57 @@
+# int
+x = 10
+# pozitivni i negativni celi brojevi
+# bez decimala
+
+# float
+y = 3.14
+# brojevi sa decimalama
+
+# complex
+z = 2 + 3j
+# koriste se u matematici (realni + imaginarni deo)
+
+# str
+ime = "Luka"
+# tekst (slova, reči, rečenice)
+
+# bool
+tacno = True
+netacno = False
+# samo dve vrednosti: True ili False
+
+# list
+lista = [1, 2, 3]
+# promenljiva (može da se menja)
+# dozvoljava duplikate
+
+# tuple
+t = (1, 2, 3)
+# ne moze da se menja
+
+# set
+s = {1, 2, 3}
+# nema duplikata, nema redosleda
+
+# dict
+osoba = {"ime": "Luka", "godine": 25}
+# ključ → vrednost (key-value)
+
+# NoneType
+x = None
+# znaci da nema vrednosti
+
+# bytes
+b = b"hello"
+# bytearray
+ba = bytearray(5)
+# memoryview
+mv = memoryview(b"hello")
+# koirste se za rad sa binarnim podacima
+
+# range
+r = range(5)
+# generise niz brojeva u ovom slucaju od 0 do 4
+
+x = 10
+print(type(x))
